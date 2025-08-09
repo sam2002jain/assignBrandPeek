7b7b7f46bcf15ec73274a395e8119ab2a4f35858
sam2002jain <jainsam1975@gmail.com>
2025-08-09 20:34:29 +0530

    unnecessary

--- screens/BrandDetails.jsx
+++ screens/BrandDetails.jsx
@@ -24,7 +24,7 @@ const BrandDetailScreen = ({ route }) => {
 
   if (isLoading) {
     return (
-      //for showing loading
+      //for showing loading state
       <LinearGradient colors={Colors.secondaryGradient} style={styles.loadingContainer}>
         <ActivityIndicator size="large" color={Colors.button} />
       </LinearGradient>
